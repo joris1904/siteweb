2549464ecce7e4d035aee232cb7e526b7131bcb7
joris1904 <joris1904@live.fr>
2016-10-30 21:40:09 +0100

    config

---
+++ config/conf.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+$data = array(
+	
+	'local' => [
+		'host' => 'localhost',
+		'base' => 'joris',
+		'user' => 'root',
+		'pass' => ''
+	],
+
+	'serveur' => [
+		'host' => 'localhost',
+		'base' => '',
+		'user' => '',
+		'pass' => ''
+	],
+
+);
